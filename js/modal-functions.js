7dc032891f97d04a71de52ac25e3cf8ef9b8898c
Bob Krstic <bobkrstic@Bobs-MacBook-Pro.local>
2019-12-10 22:52:26 -0800

    adding js to the project

---
+++ js/modal-functions.js
@@ -0,0 +1,26 @@
+function myFunction1() {
+  var x = document.getElementById("project-1-desc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("project-2-desc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("project-3-desc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
